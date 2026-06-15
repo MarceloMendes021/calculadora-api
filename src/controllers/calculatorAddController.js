1d222444fd079693a4814c041e2f9ff49ccb550d
Marcelo <marcelotmendes.ads@gmail.com>
2026-06-15 15:04:36 -0300

    fix(controllers): fix add response format and divide by zero logic

--- src/controllers/calculatorAddController.js
+++ src/controllers/calculatorAddController.js
@@ -2,12 +2,10 @@ exports.add = (req, res) => {
   const { number1, number2 } = req.body;
 
   if (!number1 || !number2) {
-    return res
-      .status(400)
-      .json({ error: "Todos os parametros são obrigatórios" });
+    return res.status(400).json({ error: "Todos os parametros são obrigatórios" });
   }
 
   const resultado = number1 + number2;
 
-  res.json(resultado);
+  res.json({ resultado });
 };
--- src/controllers/calculatorDivideController.js
+++ src/controllers/calculatorDivideController.js
@@ -1,16 +1,11 @@
 exports.divide = (req, res) => {
   const { number1, number2 } = req.body;
 
+  if (number2 === 0) {
+    return res.status(400).json({ error: "Não é possível realizar divisão por 0" });
+  }
   if (!number1 || !number2) {
-    if (number2 === 0) {
-      return res
-        .status(400)
-        .json({ error: "Não é possivel realizar divisão por 0" });
-    }
-
-    return res
-      .status(400)
-      .json({ error: "Todos os parametros são obrigatórios" });
+    return res.status(400).json({ error: "Todos os parametros são obrigatórios" });
   }
 
   const resultado = number1 / number2;
